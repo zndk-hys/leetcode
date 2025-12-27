297ac640180162c188e84015cc5859a5c0d589ca
zndk-hys <hysryt+zndk@gmail.com>
2025-12-28 03:04:05 +0900

    496 better

--- src/496.ts
+++ src/496.ts
@@ -2,38 +2,29 @@ function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
 	if (nums2.length <= 1) return Array(nums1.length).fill(-1);
 
 	const answer = Array(nums1.length).fill(-1);
-	let left = nums2.length - 2;
-	let right = nums2.length - 1;
 
-	while (left >= 0) {
-		if (nums2[left] > nums2[right]) {
-			right = left;
-			left--;
-			continue;
-		}
-
-		let found1: number | null = null;
-		for (let i = 0; i < nums1.length; i++) {
-			if (nums1[i] === nums2[left]) {
-				found1 = i;
+	for (let i = 0; i < nums1.length; i++) {
+		let matchIdx: number | null = null;
+		for (let j = 0; j < nums2.length; j++) {
+			if (nums1[i] === nums2[j]) {
+				matchIdx = j;
 				break;
 			}
 		}
 
-		if (found1 !== null) {
-			for (let i = left + 1; i <= right; i++) {
-				if (nums1[found1] < nums2[i]) {
-					answer[found1] = nums2[i];
+		if (matchIdx !== null) {
+			for (let j = matchIdx + 1; j < nums2.length; j++) {
+				if (nums1[i] < nums2[j]) {
+					answer[i] = nums2[j];
 					break;
 				}
 			}
 		}
-
-		left--;
 	}
 
 	return answer;
 }
 
 console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2])); // [-1,3,-1]
+console.log(nextGreaterElement([2, 4], [1, 2, 3, 4])); // [3,-1]
 console.log(nextGreaterElement([2, 4], [1, 2, 3, 4])); // [3,-1]
