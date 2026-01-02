5cfd0e477641a4fc208ccf20686f1dd43e74b22d
zndk-hys <hysryt+zndk@gmail.com>
2026-01-02 22:35:18 +0900

    33 better

--- src/33.ts
+++ src/33.ts
@@ -1,39 +1,30 @@
 function search(nums: number[], target: number): number {
-  const minIdx = searchMinIdx(nums);
-
   let left = 0;
-  let right = nums.length;
+  let right = nums.length - 1;
 
-  while (left < right) {
-    const idx = Math.floor((left + right) / 2);
-    const actIdx = (idx + minIdx) % nums.length;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
 
-    if (nums[actIdx] === target) {
-      return actIdx;
-    } else if (nums[actIdx] > target) {
-      right = idx;
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
     } else {
-      left = idx + 1;
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
   }
 
   return -1;
-}
-
-function searchMinIdx(nums: number[]) {
-  let left = 0;
-  let right = nums.length - 1;
-
-  while (left < right) {
-    const idx = Math.floor((left + right) / 2);
-    if (nums[idx] < nums[right]) {
-      right = idx;
-    } else {
-      left = idx + 1;
-    }
-  }
-
-  return left;
 }
 
 console.log(search([4, 5, 6, 7, 0, 1, 2], 0)); // 4
@@ -41,7 +32,3 @@ console.log(search([4, 5, 6, 7, 0, 1, 2], 3)); // -1
 console.log(search([1, 3], 3)); // 1
 console.log(search([3, 1], 3)); // 0
 console.log(search([1, 3, 5], 5)); // 2
-
-// console.log(searchMinIdx([0,1,2,3,4,5])); // 0
-// console.log(searchMinIdx([4,5,0,1,2,3])); // 2
-// console.log(searchMinIdx([2,3,4,5,0,1])); // 4
