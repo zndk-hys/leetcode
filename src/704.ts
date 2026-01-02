1af72827fe6edd1ec2f4de504aa5bd70ff146d5b
zndk-hys <hysryt+zndk@gmail.com>
2026-01-02 13:20:01 +0900

    704

---
+++ src/704.ts
@@ -0,0 +1,23 @@
+function search(nums: number[], target: number): number {
+	let left = 0;
+	let right = nums.length;
+
+	while (right > left) {
+		const idx = Math.floor((right + left) / 2);
+
+		if (nums[idx] === target) {
+			return idx;
+		} else if (nums[idx] > target) {
+			right = idx;
+		} else {
+			left = idx + 1;
+		}
+	}
+
+	return -1;
+}
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
+console.log(search([-1, 0, 3, 5, 9, 12], 2)); // -1
+console.log(search([5], 5)); // 0
+console.log(search([-1, 0, 3, 5, 9, 12], 13)); // -1
