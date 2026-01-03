f048f850d33a687c30a9bdf44d7a7edbfd284213
zndk-hys <hysryt+zndk@gmail.com>
2026-01-03 23:40:20 +0900

    141

--- src/141.ts
+++ src/141.ts
@@ -1,24 +1,27 @@
 import { arrayToList, type ListNode } from "./lib/ListNode";
 
 function hasCycle(head: ListNode | null): boolean {
-  const set = new Set<ListNode>();
+  let fast = head?.next ?? null;
+  let slow = head;
 
-  while(head) {
-    if (set.has(head)) {
+  while (fast) {
+    if (slow === fast) {
       return true;
     }
-    set.add(head);
-    head = head.next;
+    fast = fast.next?.next ?? null;
+    slow = slow?.next ?? null;
   }
 
   return false;
-};
+}
 
-const list1 = arrayToList([3,2,0,-4]);
+const list1 = arrayToList([3, 2, 0, -4]);
+// biome-ignore lint: testcode
 list1!.next!.next!.next!.next = list1!.next;
 console.log(hasCycle(list1)); // true
 
-const list2 = arrayToList([1,2]);
+const list2 = arrayToList([1, 2]);
+// biome-ignore lint: testcode
 list2!.next!.next = list2;
 console.log(hasCycle(list2)); // true
 
