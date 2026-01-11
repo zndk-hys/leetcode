b24b531597aa01eeb5156b9135958774d5d83de9
zndk-hys <hysryt+zndk@gmail.com>
2026-01-12 00:11:40 +0900

    169 better

--- src/169.ts
+++ src/169.ts
@@ -1,19 +1,18 @@
 function majorityElement(nums: number[]): number {
-  const map = new Map<number, number>();
+  let candidate = nums[0];
+  let count = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    const count = (map.get(nums[i]) ?? 0) + 1;
-
-    if (count >= nums.length / 2) {
-      return nums[i];
+    if (count === 0) {
+      candidate = nums[i];
     }
-
-    map.set(nums[i], count);
+    count += candidate === nums[i] ? 1 : -1;
   }
 
-  throw new Error();
+  return candidate;
 }
 
 console.log(majorityElement([3, 2, 3])); // 3
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // 2
-console.log(majorityElement([2, 2, 2, 1, 1])); // 2
+console.log(majorityElement([2, 2, 3, 1, 1])); // 2
+console.log(majorityElement([5, 5, 5, 7, 7, 7, 7, 1, 1, 1, 7, 7])); // 7
