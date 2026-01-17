668257a464d2022ec42c10edac014699cb662b02
zndk-hys <hysryt+zndk@gmail.com>
2026-01-18 04:45:22 +0900

    53

---
+++ src/53.ts
@@ -0,0 +1,15 @@
+function maxSubArray(nums: number[]): number {
+  let max = nums[0];
+  let current = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    current = Math.max(nums[i], current + nums[i]);
+    max = Math.max(max, current);
+  }
+
+  return max;
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+console.log(maxSubArray([1])); // 1
+console.log(maxSubArray([5, 4, -1, 7, 8])); // 23
