45be0d1e32bf7de94f5460ab63cbca6ae6ba34aa
zndk-hys <hysryt+zndk@gmail.com>
2026-01-24 00:19:02 +0900

    234 better

--- src/234.ts
+++ src/234.ts
@@ -1,23 +1,43 @@
 import { arrayToList, type ListNode } from "./lib/ListNode";
 
 function isPalindrome(head: ListNode | null): boolean {
-  const arr = [];
-  let current = head;
-  while (current) {
-    arr.push(current.val);
-    current = current.next;
+  if (head === null) {
+    return true;
   }
 
-  let left = 0;
-  let right = arr.length - 1;
-  while (left < right) {
-    if (arr[left++] !== arr[right--]) {
+  // 真ん中まで進む
+  let prev = null;
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+  while (fast?.next) {
+    fast = fast.next.next;
+    const next = slow?.next as ListNode;
+    slow.next = prev;
+    prev = slow;
+    slow = next;
+  }
+
+  // 奇数長なら一つ進める
+  if (fast !== null) {
+    slow = slow.next;
+  }
+
+  // 左右でそれぞれ進めて比較
+  let right: ListNode | null = slow;
+  let left: ListNode | null = prev;
+
+  while (right && left) {
+    if (right.val !== left.val) {
       return false;
     }
+    right = right.next;
+    left = left.next;
   }
 
   return true;
 }
 
 console.log(isPalindrome(arrayToList([1, 2, 2, 1]))); // true
+console.log(isPalindrome(arrayToList([1, 2, 1]))); // true
 console.log(isPalindrome(arrayToList([1, 2]))); // false
+console.log(isPalindrome(arrayToList([1, 0, 0]))); // false
